55d9847cfb7306e3d35680fa15caf42c16beac79
Alf Müller <alf@bastelgarage.ch>
2025-02-17 15:15:51 +0100

    refactor: Optimize Token Service with strict typing and improved method signatures

--- upload/system/library/wallee/service/token.php
+++ upload/system/library/wallee/service/token.php
@@ -1,4 +1,6 @@
 <?php
+declare(strict_types=1);
+
 /**
  * Wallee OpenCart
  *
@@ -11,6 +13,14 @@
 
 namespace Wallee\Service;
 
+use Wallee\Sdk\Model\TokenVersion;
+use Wallee\Sdk\Model\TokenVersionState;
+use Wallee\Sdk\Model\EntityQuery;
+use Wallee\Sdk\Model\EntityQueryFilter;
+use Wallee\Sdk\Model\EntityQueryFilterType;
+use Wallee\Sdk\Service\TokenService as TokenApiService;
+use Wallee\Sdk\Service\TokenVersionService as TokenVersionApiService;
+
 /**
  * This service provides functions to deal with Wallee tokens.
  */
@@ -18,39 +28,50 @@ class Token extends AbstractService {
 	
 	/**
 	 * The token API service.
-	 *
-	 * @var \Wallee\Sdk\Service\TokenService
 	 */
-	private $token_service;
+	private ?TokenApiService $token_service = null;
 	
 	/**
 	 * The token version API service.
-	 *
-	 * @var \Wallee\Sdk\Service\TokenVersionService
 	 */
-	private $token_version_service;
+	private ?TokenVersionApiService $token_version_service = null;
 
-	public function updateTokenVersion($space_id, $token_version_id){
+	/**
+	 * Updates a token version.
+	 *
+	 * @param int $space_id The space ID
+	 * @param int $token_version_id The token version ID
+	 * @return void
+	 * @throws \RuntimeException When the token version cannot be updated
+	 */
+	public function updateTokenVersion(int $space_id, int $token_version_id): void {
 		$token_version = $this->getTokenVersionService()->read($space_id, $token_version_id);
 		$this->updateInfo($space_id, $token_version);
 	}
 
-	public function updateToken($space_id, $token_id){
-		$query = new \Wallee\Sdk\Model\EntityQuery();
-		$filter = new \Wallee\Sdk\Model\EntityQueryFilter();
-		$filter->setType(\Wallee\Sdk\Model\EntityQueryFilterType::_AND);
-		$filter->setChildren(
-				array(
-					$this->createEntityFilter('token.id', $token_id),
-					$this->createEntityFilter('state', \Wallee\Sdk\Model\TokenVersionState::ACTIVE) 
-				));
+	/**
+	 * Updates a token.
+	 *
+	 * @param int $space_id The space ID
+	 * @param int $token_id The token ID
+	 * @return void
+	 * @throws \RuntimeException When the token cannot be updated
+	 */
+	public function updateToken(int $space_id, int $token_id): void {
+		$query = new EntityQuery();
+		$filter = new EntityQueryFilter();
+		$filter->setType(EntityQueryFilterType::_AND);
+		$filter->setChildren([
+			$this->createEntityFilter('token.id', $token_id),
+			$this->createEntityFilter('state', TokenVersionState::ACTIVE)
+		]);
 		$query->setFilter($filter);
 		$query->setNumberOfEntities(1);
+		
 		$token_versions = $this->getTokenVersionService()->search($space_id, $query);
 		if (!empty($token_versions)) {
 			$this->updateInfo($space_id, current($token_versions));
-		}
-		else {
+		} else {
 			$info = \Wallee\Entity\TokenInfo::loadByToken($this->registry, $space_id, $token_id);
 			if ($info->getId()) {
 				$info->delete($this->registry);
@@ -58,13 +79,19 @@ class Token extends AbstractService {
 		}
 	}
 
-	protected function updateInfo($space_id, \Wallee\Sdk\Model\TokenVersion $token_version){
+	/**
+	 * Updates the token information.
+	 *
+	 * @param int $space_id The space ID
+	 * @param TokenVersion $token_version The token version
+	 * @return void
+	 */
+	protected function updateInfo(int $space_id, TokenVersion $token_version): void {
 		$info = \Wallee\Entity\TokenInfo::loadByToken($this->registry, $space_id, $token_version->getToken()->getId());
-		if (!in_array($token_version->getToken()->getState(),
-				array(
-					\Wallee\Sdk\Model\TokenVersionState::ACTIVE,
-					\Wallee\Sdk\Model\TokenVersionState::UNINITIALIZED 
-				))) {
+		if (!in_array($token_version->getToken()->getState(), [
+			TokenVersionState::ACTIVE,
+			TokenVersionState::UNINITIALIZED
+		], true)) {
 			if ($info->getId()) {
 				$info->delete($this->registry);
 			}
@@ -74,8 +101,11 @@ class Token extends AbstractService {
 		$info->setCustomerId($token_version->getToken()->getCustomerId());
 		$info->setName($token_version->getName());
 		
-		$payment_method = \Wallee\Entity\MethodConfiguration::loadByConfiguration($this->registry, $space_id,
-				$token_version->getPaymentConnectorConfiguration()->getPaymentMethodConfiguration()->getId());
+		$payment_method = \Wallee\Entity\MethodConfiguration::loadByConfiguration(
+			$this->registry,
+			$space_id,
+			$token_version->getPaymentConnectorConfiguration()->getPaymentMethodConfiguration()->getId()
+		);
 		$info->setPaymentMethodId($payment_method->getId());
 		$info->setConnectorId($token_version->getPaymentConnectorConfiguration()->getConnector());
 		
@@ -85,33 +115,39 @@ class Token extends AbstractService {
 		$info->save();
 	}
 
-	public function deleteToken($space_id, $token_id){
+	/**
+	 * Deletes a token.
+	 *
+	 * @param int $space_id The space ID
+	 * @param int $token_id The token ID
+	 * @return void
+	 * @throws \RuntimeException When the token cannot be deleted
+	 */
+	public function deleteToken(int $space_id, int $token_id): void {
 		$this->getTokenService()->delete($space_id, $token_id);
 	}
 
 	/**
 	 * Returns the token API service.
 	 *
-	 * @return \Wallee\Sdk\Service\TokenService
+	 * @return TokenApiService
 	 */
-	protected function getTokenService(){
-		if ($this->token_service == null) {
-			$this->token_service = new \Wallee\Sdk\Service\TokenService(\WalleeHelper::instance($this->registry)->getApiClient());
+	protected function getTokenService(): TokenApiService {
+		if ($this->token_service === null) {
+			$this->token_service = new TokenApiService(\WalleeHelper::instance($this->registry)->getApiClient());
 		}
-		
 		return $this->token_service;
 	}
 
 	/**
 	 * Returns the token version API service.
 	 *
-	 * @return \Wallee\Sdk\Service\TokenVersionService
+	 * @return TokenVersionApiService
 	 */
-	protected function getTokenVersionService(){
-		if ($this->token_version_service == null) {
-			$this->token_version_service = new \Wallee\Sdk\Service\TokenVersionService(\WalleeHelper::instance($this->registry)->getApiClient());
+	protected function getTokenVersionService(): TokenVersionApiService {
+		if ($this->token_version_service === null) {
+			$this->token_version_service = new TokenVersionApiService(\WalleeHelper::instance($this->registry)->getApiClient());
 		}
-		
 		return $this->token_version_service;
 	}
 }
