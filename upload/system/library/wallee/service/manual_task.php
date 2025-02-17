13f049311a468765876b042f559b714cd24cf7d8
Alf Müller <alf@bastelgarage.ch>
2025-02-17 14:01:03 +0100

    docs: Verbessere Dokumentation der ManualTask Service-Klasse

--- upload/system/library/wallee/service/manual_task.php
+++ upload/system/library/wallee/service/manual_task.php
@@ -1,4 +1,6 @@
 <?php
+declare(strict_types=1);
+
 /**
  * Wallee OpenCart
  *
@@ -15,46 +17,80 @@ namespace Wallee\Service;
  * This service provides methods to handle manual tasks.
  */
 class ManualTask extends AbstractService {
-	const CONFIG_KEY = 'wallee_manual_task';
+	/**
+	 * Configuration key for storing the number of manual tasks.
+	 */
+	private const CONFIG_KEY = 'wallee_manual_task';
 
 	/**
 	 * Returns the number of open manual tasks.
 	 *
-	 * @return int
+	 * @return int The number of open manual tasks
 	 */
-	public function getNumberOfManualTasks(){
+	public function getNumberOfManualTasks(): int {
 		$num = $this->registry->get('config')->get(self::CONFIG_KEY);
-		return $num === null ? 0 : $num;
+		return $num === null ? 0 : (int)$num;
 	}
 
 	/**
 	 * Updates the number of open manual tasks.
 	 *
-	 * @return int
+	 * @return int The updated number of manual tasks
+	 * @throws \RuntimeException When the space ID is not configured or when the update fails
 	 */
-	public function update(){
-		$number_of_manual_tasks = 0;
-		$manual_task_service = new \Wallee\Sdk\Service\ManualTaskService(\WalleeHelper::instance($this->registry)->getApiClient());
-		
+	public function update(): int {
 		$space_id = $this->registry->get('config')->get('wallee_space_id');
-		if (!empty($space_id)) {
-			$number_of_manual_tasks = $manual_task_service->count($space_id,
-					$this->createEntityFilter('state', \Wallee\Sdk\Model\ManualTaskState::OPEN));
-			
-			$table = DB_PREFIX . 'setting';
-			$key = self::CONFIG_KEY;
-			$number_of_manual_tasks = (int) $number_of_manual_tasks;
-			$store_id = $this->registry->get('config')->get('config_store_id');
-			if($store_id === null){
-				$store_id = 0;
-			}
-			
-			\Wallee\Entity\Alert::loadManualTask($this->registry)->setCount($number_of_manual_tasks)->save();
-			
-			$this->registry->get('db')->query(
-					"UPDATE $table SET `value`='$number_of_manual_tasks' WHERE `code`='wallee' AND `key`='$key' AND `store_id`='$store_id';");
-		}
 		
-		return $number_of_manual_tasks;
+		if (empty($space_id)) {
+			throw new \RuntimeException('Space ID ist nicht konfiguriert.');
+		}
+
+		try {
+			$manual_task_service = new \Wallee\Sdk\Service\ManualTaskService(
+				\WalleeHelper::instance($this->registry)->getApiClient()
+			);
+			
+			$number_of_manual_tasks = (int)$manual_task_service->count(
+				$space_id,
+				$this->createEntityFilter('state', \Wallee\Sdk\Model\ManualTaskState::OPEN)
+			);
+
+			$this->saveManualTaskCount($number_of_manual_tasks);
+			
+			return $number_of_manual_tasks;
+		} catch (\Exception $e) {
+			throw new \RuntimeException(
+				sprintf('Fehler beim Aktualisieren der manuellen Aufgaben: %s', $e->getMessage()),
+				$e->getCode(),
+				$e
+			);
+		}
+	}
+
+	/**
+	 * Saves the manual task count to the database and updates the alert.
+	 *
+	 * @param int $count The number of manual tasks
+	 * @throws \RuntimeException When the database update fails
+	 */
+	private function saveManualTaskCount(int $count): void {
+		$table = DB_PREFIX . 'setting';
+		$store_id = (int)($this->registry->get('config')->get('config_store_id') ?? 0);
+		
+		\Wallee\Entity\Alert::loadManualTask($this->registry)
+			->setCount($count)
+			->save();
+
+		$query = sprintf(
+			"UPDATE %s SET `value`='%d' WHERE `code`='wallee' AND `key`='%s' AND `store_id`='%d';",
+			$table,
+			$count,
+			self::CONFIG_KEY,
+			$store_id
+		);
+
+		if (!$this->registry->get('db')->query($query)) {
+			throw new \RuntimeException('Fehler beim Speichern der Anzahl manueller Aufgaben in der Datenbank.');
+		}
 	}
 }
